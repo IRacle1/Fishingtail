8c91160f6af19e218772858c17137516cbba7900
IRacle <zornik2006@yandex.ru>
2024-05-01 23:00:26 +0300

    nuh

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,9 +1,7 @@
 import DefaultTheme from "vitepress/theme";
 import type { Theme } from 'vitepress'
 import "./custom.css";
-import DocsLayout from "./components/DocsLayout.vue";
 
 export default {
   extends: DefaultTheme,
-  //Layout: DocsLayout,
 } satisfies Theme
